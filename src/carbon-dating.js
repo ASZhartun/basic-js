5f6c902dad1ee8b657c10406465693e26b465381
ASZhartun <as.zhartun@gmail.com>
2022-10-13 09:30:48 +0300

    feature: add carbon-dating solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,19 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  if (sampleActivity == null) return false;
+  if (typeof sampleActivity != 'string') return false;
+  if (sampleActivity.length == 0) return false;
+  sampleActivity = +sampleActivity;
+  if (Number.isNaN(sampleActivity) || sampleActivity == 0) return false;
+/*   if (sampleActivity.match(/[0-9]+[.]?[0-9]+/) != null) return false; */
+  if (+sampleActivity > 15 || +sampleActivity <= 0) return false;
+  
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  const k = 0.693 / HALF_LIFE_PERIOD;
+  return Math.ceil(Math.log(MODERN_ACTIVITY/sampleActivity)/k);
 }
 
 module.exports = {
