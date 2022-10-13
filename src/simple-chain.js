fc9a0dce18c6853a4659ca551b2b2bb4bf1ca1cd
ASZhartun <as.zhartun@gmail.com>
2022-10-13 13:26:07 +0300

    fix: add deleting storage for object and add full solution of simple-chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,36 +5,42 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
  const chainMaker = {
-  storage : [],
+  storage : null,
   getLength() {
     return this.storage.length;
   },
   addLink(value) {
+    if (this.storage === null) this.storage = new Array();
     value = value == null ? 'null' : value;
     this.storage.push((value).toString());
     return this;
   },
   removeLink(position) {
+    if (this.storage === null) this.storage = new Array();
     position = +position;
     if (position > 0 && position < this.storage.length) {
       let temp = [];
       for (let i = 0; i < this.storage.length; i++) {
         if (i+1 != position) temp.push(this.storage[i]);
       }
-      this.storage = temp;
+      this.storage = [...temp];
+      temp = [];
       return this;
     } else {
+      this.storage = [];
       throw new Error("You can't remove incorrect link!");
     }
     
   },
   reverseChain() {
+    if (this.storage === null) this.storage = new Array();
     let temp = [];
     let len = this.storage.length;
     for (let i = 0; i < len; i++) {
       temp.push(this.storage[len - i - 1]);
     }
-    this.storage = temp;
+    this.storage = [...temp];
+    temp = [];
     return this;
   },
   finishChain() {
@@ -43,6 +49,7 @@ const { NotImplementedError } = require('../extensions/index.js');
     for (let i = 1; i < len; i++) {
       finish+=this.wrapper(this.storage[i]);
     }
+    this.storage = null;
     return finish;
   },
 
